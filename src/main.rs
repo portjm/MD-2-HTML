d1ed95db1fb1ab64595edd08c3741bea7735e5d2
portjm <jose.porta@ucf.edu>
2024-12-26 20:35:43 -0500

    Basic file type check

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ fn main() {
     if args.len() < 2 {
         eprintln!("Error: filepath not specified.\nUsage: {} <filepath>", args[0]);
         process::exit(1);
+    } 
+    let file_type = &args[1][args[1].len()-3..];
+    if file_type != ".md"{
+        eprintln!("Error: Invalid file type: {}", args[1]);
+        process::exit(1);
     }
-    
     println!("Num args: {}", args.len());
-    println!("arg 1: {}", args[0]);
+    println!("arg 1: {}", file_type);
 }
